48bcc4bc182a5a9d704f3c9a71d8cbcb162bb2b0
botinirenato <botinirenato@gmail.com>
2022-04-29 21:16:03 -0300

    Criação de Variaveis e suas Convenções

---
+++ Java Fundamental/java-fundamental/src/br/com/botidev/JavaFundamental.java
@@ -0,0 +1,17 @@
+package br.com.botidev;
+
+public class JavaFundamental {
+    public static void main(String[] args) {
+        // variaveis em java
+
+        int i; // boas praticas
+       // int i // nao deve ter o mesmo nome
+        int I; //diferente
+        // int 1; não pode comecar com numeros
+        final int  j = 10 ; // constante
+
+        int quantidadeProduto = 50; // case camel
+        final int QUANTIDADE_NUMEROS ;// SEPARADOS POR UNDERLINE
+
+    }
+}
